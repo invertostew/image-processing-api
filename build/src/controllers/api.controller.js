62b2afdcb0a681df36427bde9d555d77297850f6
Stuart Green <github.47a8z@aleeas.com>
2022-02-27 22:08:31 +0000

    feat: add ability to resize image

--- build/src/controllers/api.controller.js
+++ build/src/controllers/api.controller.js
@@ -8,11 +8,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const sharp_1 = __importDefault(require("sharp"));
 function processImage(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
+        const assetsDir = path_1.default.resolve(__dirname, '..', '..', 'assets');
+        ;
         const { filename, width, height } = req.query;
-        res.send({ filename, width, height });
+        try {
+            const file = yield (0, sharp_1.default)(`${assetsDir}/full/${filename}.jpg`)
+                .resize(width, height)
+                .toFile(`${assetsDir}/resized/${filename}-${width}x${height}.jpg`);
+            res.send('OK');
+        }
+        catch (err) {
+            res.send(err);
+        }
+        // const { filename, width, height } = req.query;
+        // try {
+        //   const file = await sharp(`${assetsPath}/full/${filename}.jpg`)
+        //     .resize(width, height)
+        //     .toFile(`${assetsPath}/resized/${filename}-${width}x${height}.jpg`);
+        //   res.sendFile(file);
+        // } catch (err) {
+        //   res.send(err);
+        // }
     });
 }
 exports.default = { processImage };
--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,10 +1,24 @@
+import path from 'path';
+
 import { Request, Response } from 'express';
 import sharp from 'sharp';
 
 async function processImage(req: Request, res: Response) {
-  const { filename, width, height } = req.query;
+  const assetsDir = path.resolve(__dirname, '..', '..', 'assets');
+  
+  const filename = req.query.filename;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
 
-  res.send({ filename, width, height });
+  try {
+    await sharp(`${assetsDir}/full/${filename}.jpg`)
+      .resize(width, height)
+      .toFile(`${assetsDir}/resized/${filename}-${width}x${height}.jpg`);
+
+    res.sendFile(`${assetsDir}/resized/${filename}-${width}x${height}.jpg`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
 export default { processImage };
