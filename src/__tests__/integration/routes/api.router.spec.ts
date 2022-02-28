d99aabaab91e423fe72d21763b3b83d1c11e67a8
Stuart Green <github.47a8z@aleeas.com>
2022-02-28 02:25:46 +0000

    test: add more endpoint tests

--- src/__tests__/integration/routes/api.router.spec.ts
+++ src/__tests__/integration/routes/api.router.spec.ts
@@ -5,22 +5,33 @@ import app from "../../../app";
 const request = supertest(app);
 
 describe("Test endpoint responses", () => {
-  it("GET /api/images with missing arguments produces 400 status code", async () => {
-    const response = await request.get("/api/images");
-    expect(response.status).toEqual(400);
-  });
+  describe("GET /api/images", () => {
+    it("GET /api/images with missing arguments produces 200 status code", async () => {
+      const response = await request.get("/api/images");
+      expect(response.status).toEqual(200);
+    });
+  
+    it("GET /api/images with missing width produces 400 status code", async () => {
+      const response = await request.get(
+        "/api/images?filename=fjord"
+      );
+      expect(response.status).toEqual(400);
+      expect(response.text).toEqual("Please append '&width=<WIDTH>' to the URL. Example: &width=500");
+    });
+  
+    it("GET /api/images with missing height produces 400 status code", async () => {
+      const response = await request.get(
+        "/api/images?filename=fjord&width=500"
+      );
+      expect(response.status).toEqual(400);
+      expect(response.text).toEqual("Please append '&height=<HEIGHT>' to the URL. Example: &height=500");
+    });
 
-  xit("GET /api/images with correct arguments produces 200 status code", async () => {
-    const response = await request.get(
-      "/api/images?filename=fjord&width=200&height=200"
-    );
-    expect(response.status).toEqual(200);
-  });
-
-  xit("GET /api/images with correct arguments in a different order produces 200 status code", async () => {
-    const response = await request.get(
-      "/api/images?width=500&filename=palmtunnel&height=500"
-    );
-    expect(response.status).toEqual(200);
+    xit("GET /api/images with correct arguments produces 200 status code", async () => {
+      const response = await request.get(
+        "/api/images?filename=fjord&width=250&height=250"
+      );
+      expect(response.status).toEqual(200);
+    });
   });
 });
--- src/middleware/validate-image-query.middleware.ts
+++ src/middleware/validate-image-query.middleware.ts
@@ -3,10 +3,26 @@ import { Request, Response, NextFunction } from "express";
 function validateImageQuery(req: Request, res: Response, next: NextFunction) {
   const { filename, width, height } = req.query;
 
-  if (!filename || !width || !height) {
-    return res.status(400).json({
-      error: 'Please provide "filename", "width" and "height" query parameters.'
-    });
+  if (Object.entries(req.query).length === 0 || !filename) {
+    const filenames = `
+      <h1>Please select an image from below:</h1>
+      <ul>
+        <li><a href="?filename=encenadaport">encenadaport</a></li>
+        <li><a href="?filename=fjord">fjord</a></li>
+        <li><a href="?filename=icelandwaterfall">icelandwaterfall</a></li>
+        <li><a href="?filename=palmtunnel">palmtunnel</a></li>
+        <li><a href="?filename=santamonica">santamonica</a></li>
+      </ul>
+    `;
+    return res.status(200).send(filenames);
+  }
+
+  if (filename && !width) {
+    return res.status(400).send("Please append '&width=<WIDTH>' to the URL. Example: &width=500");
+  }
+
+  if (filename && !height) {
+    return res.status(400).send("Please append '&height=<HEIGHT>' to the URL. Example: &height=500");
   }
 
   next();
