74923f8ba6e2062aaa3b97e4613517afb1f8c0d8
Stuart Green <github.47a8z@aleeas.com>
2022-03-02 00:34:34 +0000

    test: add endpoint test and remove test file aftereach

--- src/__tests__/integration/routes/api.router.spec.ts
+++ src/__tests__/integration/routes/api.router.spec.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs";
+
 import supertest from "supertest";
 
 import app from "../../../app";
@@ -6,6 +9,23 @@ const request = supertest(app);
 
 describe("Test endpoint responses", () => {
   describe("GET /api/images", () => {
+    afterEach(() => {
+      const testImage = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "..",
+        "..",
+        "assets",
+        "resized",
+        "fjord-250x250.jpg"
+      );
+
+      if (fs.existsSync(testImage)) {
+        fs.unlinkSync(testImage);
+      }
+    });
+
     it("GET /api/images with missing arguments produces 200 status code", async () => {
       const response = await request.get("/api/images");
       expect(response.status).toEqual(200);
@@ -30,6 +50,13 @@ describe("Test endpoint responses", () => {
     it("GET /api/images with correct arguments produces 200 status code", async () => {
       const response = await request.get(
         "/api/images?filename=fjord&width=250&height=250"
+      );
+      expect(response.status).toEqual(200);
+    });
+
+    it("GET /api/images with correct arguments (in random order) produces 200 status code", async () => {
+      const response = await request.get(
+        "/api/images?height=250&filename=fjord&width=250"
       );
       expect(response.status).toEqual(200);
     });
